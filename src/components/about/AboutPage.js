b7b4d4bc868665fbecf760d0ec716861e0a12be2
Steven Watkins <steven.watkins78@gmail.com>
2016-07-21 16:45:30 -0400

    configred layout for application

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, Redux, and React Router and a bunch of other helpful tools</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
